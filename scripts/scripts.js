0be46436d1c8b5724d67677599e4396832b04489
LilyCole <Lilia.Cole@gmail.com>
2016-09-17 01:11:32 -0700

    rearranges and cleans up

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
+	// On a click event, determine who's turn it is and have Player place marker
+	// Check if winState
+	function handleEvent(event){
+		newGame.setSpace(event);
+		newGame.makeMove(event);
+	}
+
 	// Game Constructor
 	// Should initialize a board full of null values, set first player's isTurn=true;
 	function Game(numberOfPlayers) {
@@ -7,6 +14,7 @@ $(document).ready(function() {
 		this.board=[];
 		this.players=[];
 		this.winner;
+		this.space;
 
 		// Initalizing board and first player state
 		for(var i=0;i<6;i++) {
@@ -17,7 +25,6 @@ $(document).ready(function() {
 		}
 		for(var i=0;i<numberOfPlayers;i++){
 			this.players[i]= new Player();
-			// this.players[i].identifier=i;
 		}
 		this.players[0].isTurn=true;		
 	}
@@ -36,24 +43,46 @@ $(document).ready(function() {
 		}
 	};
 
+	Game.prototype.makeMove = function(event){
+		this.setSpace(event);
+		var nextTurn;
+		if(this.players[0].isTurn) {
+			nextTurn = this.placeMarker(event,this.players[0]);
+			if(nextTurn) {
+				this.players[1].myTurn();
+				this.players[0].endTurn();
+			}
+		}
+		else {
+			nextTurn = this.placeMarker(event,this.players[1]);
+			if(nextTurn){
+				this.players[0].myTurn();
+				this.players[1].endTurn();
+			}
+		}
+		if (this.isWin()) {
+			alert(this.winner +" won!")
+		}
+	}
+
 	// Using the event, figure out which space on the board was clicked
-	Game.prototype.getSpace = function(event){
+	Game.prototype.setSpace = function(event){
 		var toParse = event.target.className;
 		var row = toParse.substring((toParse.indexOf("row_"))+4,(toParse.indexOf("row_")+5))
 		var col = toParse.substring((toParse.indexOf("col"))+4,(toParse.indexOf("col_")+5))
-		return [row-1, col-1];
+		this.space = [row-1, col-1];
 	}
 
 	// Make sure space is a valid move
-	Game.prototype.isValidMove = function(space) {
-		var row=space[0];
-		var col=space[1];
+	Game.prototype.isValidMove = function() {
+		var row=this.space[0];
+		var col=this.space[1];
 		// If space is empty
-		if (newGame.board[col][row]==null) {
+		if (this.board[col][row]==null) {
 			//If space is not in the bottom row
 			if(row!=6) {
 				// Check that spot below it is not null
-				if(newGame.board[col][row+1]==null) {
+				if(this.board[col][row+1]==null) {
 					return false;
 				}
 			}
@@ -66,13 +95,8 @@ $(document).ready(function() {
 	}
 
 	// Check if there is a Win State
-	Game.prototype.isWin = function(space) {
-		this.fourInARow();
-		this.fourInAColumn();
-		this.fourRightDiagonally();
-		this.fourLeftDiagonally();
-		if(this.winner) {
-			// this.fourInAColumn();
+	Game.prototype.isWin = function() {
+		if(this.fourInARow()||this.fourInAColumn()||this.fourLeftDiagonally()||this.fourRightDiagonally()) {
 			return true;
 		}
 		else {
@@ -143,18 +167,12 @@ $(document).ready(function() {
 		}
 		return false;
 	}
-	// Player Constructor
-	function Player() {
-		this.isTurn=false;
-		// this.identifier;
-	}
 
 	// Player places a marker on the board
-	Player.prototype.placeMarker = function(space,color,event){
-		if(newGame.isValidMove(space)) {
-			newGame.board[space[1]][space[0]] = color;
-			this.markers-=1;
-			event.target.className+=" "+color;		
+	Game.prototype.placeMarker = function(event,player){
+		if(this.isValidMove(this.space)) {
+			this.board[this.space[1]][this.space[0]] = player.color;
+			event.target.className+=" "+player.color;		
 			return true;
 		}
 		else {
@@ -162,6 +180,13 @@ $(document).ready(function() {
 			return false;
 		}
 	}
+
+	// Player Constructor
+	function Player() {
+		this.isTurn=false;
+		this.color;
+	}
+
 
 	// Changes player's turn State to true;
 	Player.prototype.myTurn = function () {
@@ -173,33 +198,13 @@ $(document).ready(function() {
 		this.isTurn=false;
 	}
 
-	// On a click event, determine who's turn it is and have Player place marker
-	// Check if winState
-	function makeMove(event){
-		space=newGame.getSpace(event);
-		var nextTurn;
-		if(newGame.players[0].isTurn) {
-			nextTurn = newGame.players[0].placeMarker(space,"red",event);
-			if(nextTurn) {
-				newGame.players[1].myTurn();
-				newGame.players[0].endTurn();
-			}
-		}
-		else {
-			nextTurn = newGame.players[1].placeMarker(space,"blue",event);
-			if(nextTurn){
-				newGame.players[0].myTurn();
-				newGame.players[1].endTurn();
-			}
-		}
-		if (newGame.isWin()) {
-			alert(newGame.winner +" won!")
-		}
-	}
 
-	// BEGIN GAME!!
+	// --> BEGIN GAME!! <-- //
 	newGame = new Game(2); //Initalize game with 2 players
-	newGame.initalizeBoard();
+	newGame.initalizeBoard(); // Adds div HTML to page
+	newGame.players[0].color="red";
+	newGame.players[1].color="blue";
 
-	$('.box').on('click',makeMove);
+	$('.box').on('click',handleEvent);
+
 })
